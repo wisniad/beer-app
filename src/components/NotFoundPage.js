f6c9aaacbff4a52570b93a2576b249039cd35d64
Damian Wisniewski <wisniad@gmail.com>
2018-05-18 20:18:15 +0200

    - Remove unnecessary import

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Flex, Box} from 'reflexbox';
 import Header from './Header';
 import Grid from '@material-ui/core/Grid';
 
 const NotFoundPage = () => (
     <div>
-
-                <Header/>
-
+        <Header/>
         <Grid container spacing={24}>
-
-        <Grid item xs={12} align="center">
+            <Grid item xs={12} align="center">
                 <h2 className="notfoundpage__paddingTop">404 Not found page</h2>
                 <h2><Link className="notfoundpage_link" to="/">Go to main page</Link></h2>
             </Grid>
